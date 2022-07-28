7ca4cb7666a07e71bba335107cdb985da2ea0fa4
Thomas Remmer <remmert765@gmail.com>
2022-07-28 16:53:26 +0100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,24 @@ const  App = () => {
       alert("please enter a todo")
       return;
     }
-    const newTodo = [input, ...list]
-    setList(newTodo)
+    setList([input, ...list])
     setNoItems(false)
   }
 
+  const handleReset = () => {
+    setList([])
+    setNoItems(true)
+  }
+
   const deleteTodo = (id) => {
-    const delArr = [...list].filter(todo => todo.id !== id)
-    setList(delArr)
+    setList([...list].filter(todo => todo.id !== id))
     if (list.length === 1) {
       setNoItems(true)
     }
   }
 
-  const completeTodo = (id) => {
-    let updatedTodos = list.map(todo => {
+  const checkBoxTicked = (id) => {
+    const updatedTodos = list.map(todo => {
       if (todo.id === id) {
         todo.complete = !todo.complete;
       }
@@ -36,24 +39,16 @@ const  App = () => {
     setList(updatedTodos);
   };
 
-  const handleReset = () => {
-    setList([])
-    setNoItems(true)
-  }
-
-  
-
   return (
     <div className="App">
       <nav>
         <h1>My Todos</h1>
         <ResetButton reset={handleReset}/>
-        
       </nav>
       <main>
         <InputBox onSubmit={addTodo}/>
         <>{noItems && <p>Nothing to see yet... Add a task in the field above!</p>}</>
-        <TodoCard userInput={list} deleteTodo={deleteTodo} completeTodo={completeTodo}/>
+        <TodoCard userInput={list} deleteTodo={deleteTodo} completeTodo={checkBoxTicked}/>
         
       </main>
     </div>
